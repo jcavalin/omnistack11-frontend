5366bd75aa0771252269825ab8fa2b7488225a0a
Julio Cavalin <julio.cavalin@gmail.com>
2020-03-28 00:09:13 -0300

    [FEAT] New features

---
+++ src/pages/Logon/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiLogIn } from 'react-icons/fi';
+
+import './styles.css';
+import heroesImg from '../../assets/heroes.png';
+import logoImg from '../../assets/logo.svg';
+
+export default function Logon() {
+    return (
+        <div className="logon-container">
+            <section className="form">
+                <img src={logoImg} alt="Be the hero" />
+
+                <form>
+                    <h1>Faça seu logo</h1>
+                    <input placeholder="Sua ID" />
+                    <button type="submit" className="button">Entrar</button>
+
+                    <Link to="/register" className="back-link">
+                        <FiLogIn size={16} color="#e02041" />
+                        Não tenho cadastro
+                    </Link>
+                </form>
+            </section>
+
+            <img src={heroesImg} alt="Heroes" />
+        </div>
+    );
+}
